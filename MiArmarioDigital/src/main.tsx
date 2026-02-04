2cd6301d66a87246b8e2a910333707341f97dbb6
Rebelan <abelcm2013@gmail.com>
2026-02-04 18:24:32 +0100

    Componente input y principio de Form de login

--- MiArmarioDigital/src/main.tsx
+++ MiArmarioDigital/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Home } from './Home'
+import { LogIn } from './LogIn'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <LogIn/>
   </StrictMode>,
 )

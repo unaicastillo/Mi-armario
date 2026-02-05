9618df6db67f188afc4e42d66c150258cffd7c73
unaicastillo <unaicastillocarrillo@gmail.com>
2026-02-05 16:59:18 +0100

    button created

--- MiArmarioDigital/src/main.tsx
+++ MiArmarioDigital/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Button } from './components/Button'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <Button nombre={'boton'}></Button>
   </StrictMode>,
 )

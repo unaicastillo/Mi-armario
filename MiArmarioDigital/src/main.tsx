ce45628365a1b6078bf4a2d2f9ce58a8db954d07
Rebelan <abelcm2013@gmail.com>
2026-02-08 15:48:18 +0100

    Register hecho

--- MiArmarioDigital/src/main.tsx
+++ MiArmarioDigital/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LogIn } from './LogIn'
+import { Register } from './Register'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='login'>
-      <LogIn/>
+      <Register/>
     </div>
   </StrictMode>,
 )

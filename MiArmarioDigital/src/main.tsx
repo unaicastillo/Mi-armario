218a399572ecd8d2187adc67718bae44673a5022
Darío.S <dariosimonrecio@gmail.com>
2026-02-08 22:37:25 +0100

    Creación de la vista Favoritos, diseño y arreglo de algunos problemas

--- MiArmarioDigital/src/main.tsx
+++ MiArmarioDigital/src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LogIn } from './LogIn'
 import { Register } from './Register'
+import { App } from './App'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='login'>
+    {/* <div className='login'>
       <Register/>
-    </div>
+    </div> */}
+    <App></App>
   </StrictMode>,
 )

0fa679d334ed40f110d382365cdb801edb62080d
Rebelan <abelcm2013@gmail.com>
2026-02-08 16:20:12 +0100

    me cago en la leche

---
+++ MiArmarioDigital/src/CreadorConjuntosPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Footer } from './components/footer'
+interface CreadorConjuntosProps{
+    
+}
+export const CreadorConjuntos = () => {
+  return (
+    <>
+    <header></header>
+    <nav>
+        <div></div>
+        <div></div>
+        <div></div>
+    </nav>
+    <Footer></Footer>
+    </>
+  )
+}
---
+++ MiArmarioDigital/src/components/form/FormRegister.tsx
@@ -0,0 +1,27 @@
+import { Button } from "../Button"
+import { Input } from "./Input"
+
+
+export const FormRegister = () => {
+    return (
+        <>
+            <div className="form-style">
+                <div className='header-form'>
+                    <h1>Inicia Sesión</h1>
+                </div>
+                <div className="inputs-form">
+                    <Input tipo="text" nombreInput="Usuario" texto="" ph="Introduce tu Usuario" />
+
+                    <Input tipo="mail" nombreInput="Correo" texto="" ph="Introduce tu correo electrónico" />
+
+                    <Input tipo="password" nombreInput="Contraseña" texto="" ph="Introduce tu contraseña" />
+                    
+                    <Input tipo="password" nombreInput="Contraseña" texto="" ph="Repite la contraseña" />
+
+                    <Button nombre="Registrarse" />
+                </div>
+            </div>
+
+        </>
+    )
+}

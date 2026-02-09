beb138b3ca84d995f8d310ded0dbf75982acd4d6
unaicastillo <unaicastillocarrillo@gmail.com>
2026-02-09 12:42:31 +0100

    button finished

--- MiArmarioDigital/src/App.tsx
+++ MiArmarioDigital/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Boton_tema } from './components/Boton_tema'
-import { Footer } from './components/footer'
+import { Footer } from './components/Footer'
+import { Button } from './components/Button'
+import './index.css'
 
 export const App = () => {
   return (
     <>
     <Boton_tema></Boton_tema>
     <Footer></Footer>
+    <Button nombre={'Enviar'} class={'btn'}></Button>
     </>
   )
 }

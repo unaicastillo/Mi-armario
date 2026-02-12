f7b48082c3b2e8ccd4a9bf26f87d07bde54f0197
unaicastillo <unaicastillocarrillo@gmail.com>
2026-02-12 20:55:35 +0100

    paginas añadidas a la carpeta pages

--- MiArmarioDigital/src/pages/CreadorConjuntosPage.tsx
+++ MiArmarioDigital/src/pages/CreadorConjuntosPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/Header'
-import { Button } from './components/Button'
-import { Footer } from './components/footer'
+import Header from '../components/Header'
+import { Button } from '../components/Button'
+import { Footer } from '../components/footer'
 
 export const CreadorConjuntos = () => {
   return (
--- MiArmarioDigital/src/pages/MostrarRopaPage.tsx
+++ MiArmarioDigital/src/pages/MostrarRopaPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/Header'
-import { Footer } from './components/footer'
-import { Button } from './components/Button'
+import Header from '../components/Header'
+import { Footer } from '../components/footer'
+import { Button } from '../components/Button'
 
 export const MostrarRopa = () => {
   return (

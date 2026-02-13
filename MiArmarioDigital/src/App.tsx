2dda2eedd629b8b3dc4f994f0dbd25199889bb88
mpozor15 <manuelpozor2004@gmail.com>
2026-02-13 20:42:51 +0100

    App error

--- MiArmarioDigital/src/App.tsx
+++ MiArmarioDigital/src/App.tsx
@@ -3,11 +3,12 @@ import { Boton_tema } from './components/Boton_tema'
 import { Footer } from './components/footer'
 import { Favoritos } from './pages/Favoritos'
 import { Button } from './components/Button'
+import LandingUsuarioIniciado from './pages/LandingUsuarioIniciadoPage'
 
 export const App = () => {
   return (
     <>
-    <Footer></Footer>
+    
     </>
   )
 }

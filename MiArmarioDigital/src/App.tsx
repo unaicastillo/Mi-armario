461484861021c8b7ac04b4b5bb2f07ed9fb7eb8e
mpozor15 <manuelpozor2004@gmail.com>
2026-03-05 16:37:57 +0100

    Registro Responsive

--- MiArmarioDigital/src/App.tsx
+++ MiArmarioDigital/src/App.tsx
@@ -4,6 +4,7 @@ import { Footer } from './components/footer'
 import { Favoritos } from './pages/Favoritos'
 import { Button } from './components/Button'
 import LandingUsuarioIniciado from './pages/LandingUsuarioIniciadoPage'
+import Header from './components/Header'
 
 export const App = () => {
   return (

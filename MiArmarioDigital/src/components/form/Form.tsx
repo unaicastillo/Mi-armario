c94e326d8c3320487201cfad37422d92bfdb24fd
Rebelan <abelcm2013@gmail.com>
2026-03-05 16:59:50 +0100

    nombre clase del boton de form

--- MiArmarioDigital/src/components/form/Form.tsx
+++ MiArmarioDigital/src/components/form/Form.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { Input } from './Input'
 import { Button } from '../Button'
-import { Link } from 'react-router-dom'
 
 export const Form = () => {
     return (
         <>
             <div className="form-style">
                 <div className='header-form'>
-                    <h1 className="text-2xl sm:text-3xl">Inicia Sesión</h1>
+                    <h1>Inicia Sesión</h1>
                 </div>
                 <div className="inputs-form">
                     <Input tipo="mail" nombreInput="Correo" texto="" ph="Introduce tu correo electrónico" />
                     <Input tipo="password" nombreInput="Contraseña" texto="" ph="Introduce tu contraseña" />
-                    <Link to="/home" className="w-full">
-                        <Button nombre="Iniciar sesion" class="header-btn-login w-full"/>
-                    </Link>
+
+                    <Button nombre="Iniciar sesion" class="btn"/>
                 </div>
-                <a href="" className="text-sm sm:text-base hover:underline">¿Has olvidado tu contraseña?</a>
+                <a href="">¿Has olvidado tu contraseña?</a>
             </div>
 
         </>

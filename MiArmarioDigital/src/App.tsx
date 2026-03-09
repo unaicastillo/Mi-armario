e14fc374bf65619de5d10aedc8cfaff9c4d62ca8
Darío.S <dariosimonrecio@gmail.com>
2026-03-09 20:30:02 +0100

    Implementación de sugerencias de mejora en revisión de código de parte de Abel: -Cambio de la targeta de conjunto e incluido el boton favoritos -Creación del elemento Button_Favoritos y su diseño (la funcionalidad se 	implementará posteriormente). -Agregados los iconos de favoritos (empty_star.ico y full_star.ico) Patron estrellas de fondo para la vista favoritos.
    A parte se a hecho un cambio de class a ClassName dentro de los
    	botones del header para que se muestre su apariencia.

--- MiArmarioDigital/src/App.tsx
+++ MiArmarioDigital/src/App.tsx
@@ -3,13 +3,19 @@ import { Boton_tema } from './components/Boton_tema'
 import { Footer } from './components/footer'
 import { Favoritos } from './pages/Favoritos'
 import { Button } from './components/Button'
-import LandingUsuarioIniciado from './pages/LandingUsuarioIniciadoPage'
-import Header from './components/Header'
+import { Register } from './Register'
+import Perfil from './pages/Perfil'
+import { LogIn } from './LogIn'
 
 export const App = () => {
   return (
     <>
-    
+    {/* <Register></Register> */}
+    {/* <Perfil></Perfil> */}
+    {/* <LogIn></LogIn> */}
+    <Favoritos></Favoritos>
+    {/* <Footer></Footer>
+    <Button nombre={'Enviar'} class={'btn'}></Button> */}
     </>
   )
 }
